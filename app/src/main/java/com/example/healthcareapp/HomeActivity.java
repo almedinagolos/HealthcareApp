5efbfd28e35eae486f773d31850c052d5a5481a7
Almedina GT <almedina.golos@gmail.com>
2023-10-09 13:21:29 +0200

    Update home page

--- app/src/main/java/com/example/healthcareapp/HomeActivity.java
+++ app/src/main/java/com/example/healthcareapp/HomeActivity.java
@@ -1,8 +1,14 @@
 package com.example.healthcareapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +16,21 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //welcome message for user, we've already save username in local memory in the login activity
+        SharedPreferences sharedPreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+        String username = sharedPreferences.getString("username", "").toString();
+        Toast.makeText(getApplicationContext(), "Welcome " + username, Toast.LENGTH_SHORT).show();
+
+        CardView exit = findViewById(R.id.cardLogout);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.clear(); //we are cleaning all key value information (logg user)
+                editor.apply(); // apply changes
+                startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
